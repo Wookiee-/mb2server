8c7cc02cc888b44e2df5f7d2a1f3789dd3f21b2a
Wookiee- <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>
2024-08-11 07:54:53 +0000

    Test

---
+++ install.sh
@@ -0,0 +1,281 @@
+#!/bin/bash
+# submenu
+
+#get script path here
+SCRIPT=$(readlink -f $0)
+SCRIPTPATH=`dirname $SCRIPT`
+OPENJKPATH="$HOME/openjk"
+MBIIPATH="$OPENJKPATH/MBII"
+MACHINE_TYPE=`uname -m`
+
+cd $SCRIPTPATH
+
+debian () {
+  local PS3=$'\nPlease enter sub option: '
+  local options=("Dependancies" "Python Tools" "MBII Server" "RTVRTM" "Dotnet" "MBII Updater" "Update MBII" "Back to main menu")
+  local opt
+  select opt in "${options[@]}"
+  do
+      case $opt in
+          "Dependancies")
+	if [ ${MACHINE_TYPE} == 'x86_64' ]; then
+		sudo dpkg --add-architecture i386
+                sudo apt-get update
+		sudo apt-get install -y libc6:i386 libncurses5:i386 libstdc++6:i386 zlib1g:i386 curl:i386 lib32z1 build-essential cmake gcc-multilib g++-multilib libjpeg-dev:i386 libpng-dev:i386 zlib1g-dev:i386 libcurl4:i386
+	else
+                sudo apt-get update
+		sudo apt-get install -y libc6:i386 libncurses5:i386 libstdc++6:i386 zlib1g:i386 curl:i386 lib32z1 build-essential cmake gcc-multilib g++-multilib libjpeg-dev:i386 libpng-dev:i386 zlib1g-dev:i386 libcurl4:i386 
+	fi
+		debian;
+              ;;
+          "Python Tools")
+		sudo apt-get update
+		sudo apt install -y build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev curl libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
+	if [ ! -r ~/.pyenv/ ]
+			then
+    		git clone https://github.com/pyenv/pyenv.git ~/.pyenv
+    		git clone https://github.com/pyenv/pyenv-virtualenv.git ~/.pyenv/plugins/pyenv-virtualenv
+    		echo '
+	# pyenv
+		export PYENV_ROOT="$HOME/.pyenv"
+		export PATH="$PYENV_ROOT/bin:$PATH"
+	if command -v pyenv 1>/dev/null 2>&1; then
+    		eval "$(pyenv init -)"
+    		eval "$(pyenv virtualenv-init -)" # Enable auto-activation of virtualenvs
+	fi' >> ~/.bashrc
+    		source ~/.bashrc
+    		exec "$SHELL"
+	fi
+		pyenv install 2.7.18 -s -v
+		pyenv local 2.7.18
+		debian;
+		;;
+          "MBII Server")
+ 	if [ -d $MBIIPATH ]; then
+		clear;
+       		sleep 2
+	else
+        	clear;
+        	sleep 2
+
+        #Download file lists, get the latest
+        wget -O "$SCRIPTPATH/downloads" https://archive.moviebattles.org/releases/
+
+        while IFS= read -r line; do
+
+                SUB='FULL'
+                if [[ "$line" == *"$SUB"* ]]; then
+                  FILENAME=`echo "$line" | grep -io '<a href=['"'"'"][^"'"'"']*['"'"'"]' | sed -e 's/^<a href=["'"'"']//i' -e 's/["'"'"']$//i'`
+                  LINK="https://archive.moviebattles.org/releases/$FILENAME"
+                fi
+        done < downloads
+
+      		wget -O "$SCRIPTPATH/MBII.zip" $LINK
+       		unzip -o MBII.zip -d $OPENJKPATH
+		rm MBII.zip
+		cd $MBIIPATH
+
+		mv -f jampgamei386.so jampgamei386.jamp.so
+		cp jampgamei386.nopp.so jampgamei386.so
+		
+		mkdir -p $HOME/.local/share/openjk/
+		ln -s $HOME/openjk $HOME/.local/share/openjk/
+		
+		chmod +x $OPENJKPATH/mbiided.i386		
+
+	fi
+                debian;
+              ;;
+          "RTVRTM")
+		cd $SCRIPTPATH
+		
+		cp rtvrtm.py $OPENJKPATH/  
+		chmod +x $OPENJKPATH/rtvrtm.py
+                debian;
+              ;;
+          "Dotnet")
+		sudo add-apt-repository ppa:dotnet/backports
+                sudo apt-get update
+                sudo apt-get install -y apt-transport-https dotnet-sdk-6.0
+                debian;
+              ;;
+          "MBII Updater")
+                wget https://www.moviebattles.org/download/MBII_CLI_Updater.zip
+                unzip -o MBII_CLI_Updater.zip -d $OPENJKPATH
+                rm MBII_CLI_Updater.zip
+                debian;
+              ;;
+          "Update MBII")
+                cd $OPENJKPATH
+                dotnet MBII_CommandLine_Update_XPlatform.dll
+
+                mv -f jampgamei386.so jampgamei386.jamp.so
+                cp jampgamei386.nopp.so jampgamei386.so
+                debian;
+              ;;
+          "Back to main menu")
+              main;
+              ;;
+          *) echo "invalid option $REPLY";;
+      esac
+  done
+}
+
+ubuntu () {
+  local PS3=$'\nPlease enter sub option: '
+  local options=("Dependancies" "Python Tools" "MBII Server" "RTVRTM" "Dotnet" "MBII Updater" "Update MBII" "Back to main menu")
+  local opt
+  select opt in "${options[@]}"
+  do
+      case $opt in
+          "Dependancies")
+	if [ ${MACHINE_TYPE} == 'x86_64' ]; then
+		sudo dpkg --add-architecture i386
+                sudo apt-get update
+		sudo apt-get install -y libc6:i386 libncurses6:i386 libstdc++6:i386 zlib1g:i386 curl:i386 lib32z1 build-essential cmake gcc-multilib g++-multilib libjpeg-dev:i386 libpng-dev:i386 zlib1g-dev:i386 libcurl4:i386
+ 	else
+                sudo apt-get update
+		sudo apt-get install -y libc6:i386 libncurses6:i386 libstdc++6:i386 zlib1g:i386 curl:i386 lib32z1 build-essential cmake gcc-multilib g++-multilib libjpeg-dev:i386 libpng-dev:i386 zlib1g-dev:i386 libcurl4:i386
+	fi
+            ubuntu;
+              ;;
+          "Python Tools")
+		sudo apt-get update
+                sudo apt install -y build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev curl libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
+        if [ ! -r ~/.pyenv/ ]
+                        then
+                git clone https://github.com/pyenv/pyenv.git ~/.pyenv
+                git clone https://github.com/pyenv/pyenv-virtualenv.git ~/.pyenv/plugins/pyenv-virtualenv
+                echo '
+        # pyenv
+                export PYENV_ROOT="$HOME/.pyenv"
+                export PATH="$PYENV_ROOT/bin:$PATH"
+        if command -v pyenv 1>/dev/null 2>&1; then
+                eval "$(pyenv init -)"
+                eval "$(pyenv virtualenv-init -)" # Enable auto-activation of virtualenvs
+        fi' >> ~/.bashrc
+                source ~/.bashrc
+                exec "$SHELL"
+        fi
+                pyenv install 2.7.18 -s -v
+                pyenv local 2.7.18
+            ubuntu;
+              ;;
+          "MBII Server")
+ 	if [ -d $MBIIPATH ]; then
+		clear;
+       		sleep 2
+	else
+        	clear;
+        	sleep 2
+
+        #Download file lists, get the latest
+        wget -O "$SCRIPTPATH/downloads" https://archive.moviebattles.org/releases/
+
+        while IFS= read -r line; do
+
+                SUB='FULL'
+                if [[ "$line" == *"$SUB"* ]]; then
+                  FILENAME=`echo "$line" | grep -io '<a href=['"'"'"][^"'"'"']*['"'"'"]' | sed -e 's/^<a href=["'"'"']//i' -e 's/["'"'"']$//i'`
+                  LINK="https://archive.moviebattles.org/releases/$FILENAME"
+                fi
+        done < downloads
+
+      		wget -O "$SCRIPTPATH/MBII.zip" $LINK
+       		unzip -o MBII.zip -d $OPENJKPATH
+		rm MBII.zip
+		cd $MBIIPATH
+
+		mv -f jampgamei386.so jampgamei386.jamp.so
+		cp jampgamei386.nopp.so jampgamei386.so
+		
+		mkdir -p $HOME/.local/share/openjk/
+		ln -s $HOME/openjk $HOME/.local/share/openjk/
+		
+		chmod +x $OPENJKPATH/mbiided.i386			
+
+	fi
+              ;;
+          "RTVRTM")
+		cd $SCRIPTPATH
+		
+		cp rtvrtm.py $OPENJKPATH/  
+		chmod +x $OPENJKPATH/rtvrtm.py
+            ubuntu;
+              ;;
+          "Dotnet")
+                sudo add-apt-repository ppa:dotnet/backports
+                sudo apt-get update
+                sudo apt-get install -y apt-transport-https dotnet-sdk-6.0
+            ubuntu;
+              ;;
+          "MBII Updater")
+                wget https://www.moviebattles.org/download/MBII_CLI_Updater.zip
+                unzip -o MBII_CLI_Updater.zip -d $OPENJKPATH
+                rm MBII_CLI_Updater.zip
+            ubuntu;
+              ;;
+          "Update MBII")
+                cd $OPENJKPATH
+                dotnet MBII_CommandLine_Update_XPlatform.dll
+
+                cd $MBIIPATH
+                mv -f jampgamei386.so jampgamei386.jamp.so
+                cp jampgamei386.nopp.so jampgamei386.so
+            ubuntu;
+              ;;
+          "Back to main menu")
+              main;
+              ;;
+          *) echo "invalid option $REPLY";;
+      esac
+  done
+}
+
+main () {
+local PS3=$'\nEnter a number and press enter: '
+local options=("Debian" "Ubuntu" "Quit")
+local opt
+select opt in "${options[@]}"
+do
+    case $opt in
+        "Debian")
+            debian
+            ;;
+        "Ubuntu")
+            ubuntu
+            ;;
+        "Quit")
+            exit
+            ;;
+        *) echo "invalid option $REPLY";;
+    esac
+done
+}
+
+
+# main menu
+echo "*************************************************"
+echo " 	   Moviebattles II EZ Installer Tool           "
+echo "        	  Interactive Menu                     "
+echo "*************************************************"
+echo ""
+echo "		Press a number to install 	       "
+
+PS3=$'\nEnter a number and press enter: '
+options=("Debian" "Ubuntu" "Quit")
+select opt in "${options[@]}"
+do
+    case $opt in
+        "Debian")
+            debian
+            ;;
+        "Ubuntu")
+            ubuntu
+            ;;
+        "Quit")
+            exit
+            ;;
+        *) echo "invalid option $REPLY";;
+    esac
+done
